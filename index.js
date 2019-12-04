7e9134db7aeb2e346a9ee1679b28fd733f8763b5
erica2605@live.com <erica2605@live.com>
2019-12-04 15:51:07 -0800

    readable values challenge complete

--- index.js
+++ index.js
@@ -1,5 +1,38 @@
-class DateLib {
+class EasyDate {
     constructor(...args) {
         this.date = new Date(...args)
     }
+
+    year() {
+        return this.date.getFullYear();
+    }
+
+    month() {
+        return this.date.getMonth();
+    }
+
+    day() {
+        return this.date.getDate();
+    }
+
+    hours() {
+        return this.date.getHours();
+    }
+
+    minutes() {
+        return this.date.getMinutes();
+    }
+
+    seconds() {
+        return this.date.getSeconds();
+    }
+
+    milliseconds() {
+        return this.date.getMilliseconds();
+    }
+
+    getTime() {
+        return this.date.getTime();
+    }
+
 }
